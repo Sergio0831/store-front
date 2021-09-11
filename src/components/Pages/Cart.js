7391b1f9f5511018dd8ae7b0e625da2fb97068f8
Sergio0831 <sergejs.ivcenko@gmail.com>
2021-09-11 09:53:35 +0100

    Implemented cart page

--- src/components/Pages/Cart.js
+++ src/components/Pages/Cart.js
@@ -1,12 +1,56 @@
 import React from "react";
 
-const Cart = () => {
+const Cart = ({ cart }) => {
+  const totalCartPrice = cart.reduce((total, product) => {
+    const totalPrice = product.price * product.quantity;
+    total += totalPrice;
+    return total;
+  }, 0);
+
   return (
     <div className='cart-layout'>
-      <div>
-        <h1>Your Cart</h1>
+      {cart.length === 0 ? (
         <p>You have not added any product to your cart yet.</p>
-      </div>
+      ) : (
+        <table className='table table-cart'>
+          <thead>
+            <tr>
+              <th width='25%' className='th-product'>
+                Product
+              </th>
+              <th width='20%'>Unit price</th>
+              <th width='10%'>Quanity</th>
+              <th width='25%'>Total</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cart.map((product) => {
+              const { id, name, price, quantity, image } = product;
+              const totalPrice = quantity * price;
+              return (
+                <tr key={id}>
+                  <td>
+                    <img width='30' height='30' alt='' src={image} alt={name} />
+                    {name}
+                  </td>
+                  <td>&euro;{price}</td>
+                  <td>{quantity}</td>
+                  <td>
+                    <strong>&euro;{totalPrice}</strong>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+          <tfoot>
+            <tr>
+              <th colSpan='2'></th>
+              <th className='cart-highlight'>Total</th>
+              <th className='cart-highlight'>&euro;{totalCartPrice}</th>
+            </tr>
+          </tfoot>
+        </table>
+      )}
     </div>
   );
 };
